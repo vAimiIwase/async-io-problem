4174e2a84d8ba455501e88afde5cdfa46aafd9b8
岩瀬 愛美 <aimi.iwase@Veriserve.co.jp>
2020-04-03 16:16:29 +0900

    宿題を実施

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 500; count++) {
-  fs.appendFile(fileName, 'あ', 'utf8', () => {});
-  fs.appendFile(fileName, 'い', 'utf8', () => {});
-  fs.appendFile(fileName, 'う', 'utf8', () => {});
-  fs.appendFile(fileName, 'え', 'utf8', () => {});
-  fs.appendFile(fileName, 'お', 'utf8', () => {});
-  fs.appendFile(fileName, '\n', 'utf8', () => {});
+for (let count = 0; count < 30; count++) {
+    fs.appendFileSync(fileName, 'おはようございます\n', 'utf8');
+    fs.appendFileSync(fileName, 'こんにちは\n', 'utf8');
+    fs.appendFileSync(fileName, 'こんばんは\n', 'utf8');
 }
